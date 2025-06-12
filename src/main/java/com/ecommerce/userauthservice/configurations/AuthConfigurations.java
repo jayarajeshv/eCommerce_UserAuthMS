78c24a05ed1fb77cb601ade198201d420402a83b
jayarajeshv <jayarajeshkumar.v@outlook.com>
2025-06-12 21:58:41 +0100

    Added Vanilla Security Config Details from https://docs.spring.io/spring-authorization-server/reference/getting-started.html

--- src/main/java/com/ecommerce/userauthservice/configurations/AuthConfigurations.java
+++ src/main/java/com/ecommerce/userauthservice/configurations/AuthConfigurations.java
@@ -13,22 +13,22 @@ public class AuthConfigurations {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> {
-                            try {
-                                requests
-                                        .anyRequest().permitAll()
-                                        .and().cors().disable()
-                                        .csrf().disable();
-                            } catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                );
-
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((requests) -> {
+//                            try {
+//                                requests
+//                                        .anyRequest().permitAll()
+//                                        .and().cors().disable()
+//                                        .csrf().disable();
+//                            } catch (Exception e) {
+//                                throw new RuntimeException(e);
+//                            }
+//                        }
+//                );
+//
+//        return http.build();
+//    }
 
 }
