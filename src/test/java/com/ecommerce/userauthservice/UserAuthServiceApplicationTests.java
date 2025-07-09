84dc4d497d17518ddd04daf3286c31e68124c898
jayarajeshv <jayarajeshkumar.v@outlook.com>
2025-07-09 22:31:38 +0100

    for client authorizations

--- src/test/java/com/ecommerce/userauthservice/UserAuthServiceApplicationTests.java
+++ src/test/java/com/ecommerce/userauthservice/UserAuthServiceApplicationTests.java
@@ -44,6 +44,7 @@ class UserAuthServiceApplicationTests {
                     .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                     .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                     .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                    .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
                     .redirectUri("https://oauth.pstmn.io/v1/callback")
                     .postLogoutRedirectUri("https://oauth.pstmn.io/v1/callback")
                     .scope("ADMIN")
